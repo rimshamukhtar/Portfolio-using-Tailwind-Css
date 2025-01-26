6e23c9121b0cee771ee2128e23a3dfd3c1e4c8ed
RimshaMukhtar <160596706+rimshamukhtar@users.noreply.github.com>
2025-01-26 16:31:47 +0500

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,11 +1,10 @@
-
 import React from 'react'
 import { FaHtml5, FaCss3Alt, FaJs } from 'react-icons/fa'
 
 const Skills = () => {
   return (
-    <section className='text-white '>
-      <div className='container mx-auto flex flex-col items-center h-screen justify-center gap-6 font-serif text-cyan-200'>
+    <section className='text-white'>
+      <div className='container mx-auto flex flex-col items-center justify-center h-screen gap-6 font-serif text-cyan-200'>
         
         <h1 className='text-center font-bold text-xl border-2 border-cyan-600 bg-cyan-700 hover:bg-cyan-800 text-white p-2 rounded-lg w-36 shadow-lg transition-all duration-300'>
           Skills
@@ -15,17 +14,18 @@ const Skills = () => {
           What I Can Do
         </h2>
 
-    
-        <div className='flex items-center justify-center w-80 h-14 text-center font-bold text-xl p-2 rounded-full shadow-lg border-2 border-cyan-800 bg-cyan-50 text-cyan-900 transform hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-cyan-600'>
-          <FaHtml5 className="text-3xl mr-3 text-red-600 animate-pulse" /> HTML
-        </div>
+        <div className='flex flex-col sm:flex-row sm:gap-6 items-center justify-center'>
+          <div className='flex items-center justify-center w-80 h-14 text-center font-bold text-xl p-2 rounded-full shadow-lg border-2 border-cyan-800 bg-cyan-50 text-cyan-900 transform hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-cyan-600'>
+            <FaHtml5 className="text-3xl mr-3 text-red-600 animate-pulse" /> HTML
+          </div>
 
-        <div className='flex items-center justify-center w-80 h-14 text-center font-bold text-xl p-2 rounded-full shadow-lg border-2 border-cyan-800 bg-cyan-50 text-cyan-900 transform hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-cyan-600'>
-          <FaCss3Alt className="text-3xl mr-3 text-blue-600 animate-pulse" /> CSS
-        </div>
+          <div className='flex items-center justify-center w-80 h-14 text-center font-bold text-xl p-2 rounded-full shadow-lg border-2 border-cyan-800 bg-cyan-50 text-cyan-900 transform hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-cyan-600'>
+            <FaCss3Alt className="text-3xl mr-3 text-blue-600 animate-pulse" /> CSS
+          </div>
 
-        <div className='flex items-center justify-center w-80 h-14 text-center font-bold text-xl p-2 rounded-full shadow-lg border-2 border-cyan-800 bg-cyan-50 text-cyan-900 transform hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-cyan-600'>
-          <FaJs className="text-3xl mr-3 text-yellow-500 animate-pulse" /> JavaScript
+          <div className='flex items-center justify-center w-80 h-14 text-center font-bold text-xl p-2 rounded-full shadow-lg border-2 border-cyan-800 bg-cyan-50 text-cyan-900 transform hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-cyan-600'>
+            <FaJs className="text-3xl mr-3 text-yellow-500 animate-pulse" /> JavaScript
+          </div>
         </div>
 
       </div>
