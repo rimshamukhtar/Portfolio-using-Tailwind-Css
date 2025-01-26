c52e017ab806c727f7a44ba0ebc5e0920c116e45
RimshaMukhtar <160596706+rimshamukhtar@users.noreply.github.com>
2025-01-26 16:30:31 +0500

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import Image from 'next/image'
 import ATM from "../../public/ATM pic.jpg"
@@ -10,80 +9,69 @@ import wordcounter from "../../public/word counter.png"
 
 const Projects = () => {
   return (
-    <div className="container mx-auto text-white flex flex-col items-center justify-center gap-4">
-      <h1 className='font-extrabold text-center text-2xl font-serif border-2 border-cyan-800 bg-cyan-600 hover:bg-cyan-700 text-white p-2 rounded-lg w-40'>
+    <div className="container mx-auto text-white flex flex-col items-center justify-center gap-6 px-4">
+      {/* Title */}
+      <h1 className="font-extrabold text-center text-2xl font-serif border-2 border-cyan-800 bg-cyan-600 hover:bg-cyan-700 text-white p-2 rounded-lg w-48 sm:w-64">
         My Projects
       </h1>
-      
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-      
-        <div className='w-60 h-72 border-2 border-cyan-950 bg-cyan-800 flex flex-col rounded-sm transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-          <div className='h-40'>
-            <Image src={ATM} alt="ATM Machine" width={400} height={400} className="bg-transparent shadow-md w-60 h-40 rounded-sm"/>
-          </div>
-          <div className='flex flex-col p-2'>
-            <h1 className='font-bold font-serif text-center'>ATM Machine</h1>
-            <p className='font-serif text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati excepturi</p>
-          </div>
-        </div>
 
-      
-        <div className='w-60 h-72 border-2 border-cyan-950 bg-cyan-800 flex flex-col rounded-sm transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-          <div className='h-40'>
-            <Image src={calculator} alt="Simple Calculator" width={400} height={400} className="bg-transparent shadow-md w-60 h-40 rounded-sm"/>
-          </div>
-          <div className='flex flex-col p-2'>
-            <h1 className='font-bold font-serif text-center'>Simple Calculator</h1>
-            <p className='font-serif text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati excepturi</p>
-          </div>
-        </div>
+      {/* Project Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {/* Project Card */}
+        {[{
+          image: ATM,
+          title: "ATM Machine",
+          description: "An interactive ATM simulation project.",
+        }, {
+          image: calculator,
+          title: "Simple Calculator",
+          description: "A basic calculator for arithmetic operations.",
+        }, {
+          image: currencyconverter,
+          title: "Currency Converter",
+          description: "Easily convert currencies in real-time.",
+        }, {
+          image: numberguessing,
+          title: "Number Guessing Game",
+          description: "A fun game to guess numbers within range.",
+        }, {
+          image: todolist,
+          title: "To-do List",
+          description: "Keep track of daily tasks effectively.",
+        }, {
+          image: wordcounter,
+          title: "Word Counter",
+          description: "Count words and characters in real-time.",
+        }].map((project, index) => (
+          <div
+            key={index}
+            className="w-full max-w-sm mx-auto border-2 border-cyan-950 bg-cyan-800 flex flex-col rounded-sm transition-transform transform hover:-translate-y-2 hover:scale-105 duration-300"
+          >
+            {/* Image */}
+            <div className="h-40 w-full overflow-hidden">
+              <Image
+                src={project.image}
+                alt={project.title}
+                width={400}
+                height={400}
+                className="w-full h-full object-cover"
+              />
+            </div>
 
-      
-        <div className='w-60 h-72 border-2 border-cyan-950 bg-cyan-800 flex flex-col rounded-sm transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-          <div className='h-40'>
-            <Image src={currencyconverter} alt="Currency Converter" width={400} height={400} className="bg-transparent shadow-md w-60 h-40 rounded-sm"/>
+            {/* Content */}
+            <div className="flex flex-col p-4">
+              <h1 className="font-bold font-serif text-center text-lg">
+                {project.title}
+              </h1>
+              <p className="font-serif text-center text-sm text-gray-300">
+                {project.description}
+              </p>
+            </div>
           </div>
-          <div className='flex flex-col p-2'>
-            <h1 className='font-bold font-serif text-center'>Currency Converter</h1>
-            <p className='font-serif text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati excepturi</p>
-          </div>
-        </div>
-       
-        <div className='w-60 h-72 border-2 border-cyan-950 bg-cyan-800 flex flex-col rounded-sm transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-          <div className='h-40'>
-            <Image src={numberguessing} alt="Number Guessing Game" width={400} height={400} className="bg-transparent shadow-md w-60 h-40 rounded-sm"/>
-          </div>
-          <div className='flex flex-col p-2'>
-            <h1 className='font-bold font-serif text-center'>Number Guessing Game</h1>
-            <p className='font-serif text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati excepturi</p>
-          </div>
-        </div>
-
-       
-        <div className='w-60 h-72 border-2 border-cyan-950 bg-cyan-800 flex flex-col rounded-sm transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-          <div className='h-40'>
-            <Image src={todolist} alt="To-do List" width={400} height={400} className="bg-transparent shadow-md w-60 h-40 rounded-sm"/>
-          </div>
-          <div className='flex flex-col p-2'>
-            <h1 className='font-bold font-serif text-center'>To-do List</h1>
-            <p className='font-serif text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati excepturi</p>
-          </div>
-        </div>
-
-      
-        <div className='w-60 h-72 border-2 border-cyan-950 bg-cyan-800 flex flex-col rounded-sm transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-          <div className='h-40'>
-            <Image src={wordcounter} alt="Word Counter" width={400} height={400} className="bg-transparent shadow-md w-60 h-40 rounded-sm"/>
-          </div>
-          <div className='flex flex-col p-2'>
-            <h1 className='font-bold font-serif text-center'>Word Counter</h1>
-            <p className='font-serif text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati excepturi</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
 }
 
 export default Projects
-
